c37e399ccd517e99220c8bdef4e5a298a68d21dc
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-04-02 21:44:21 -0700

    added a userslist page and relevant components

--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+import dummyUserImage from "../../assets/images/dummy-user.jpg";
+
 const Users = () => {
-  return <div>Users</div>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "dummy user",
+      image: dummyUserImage,
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
