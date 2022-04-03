011190dca04f02f5885e2c7aa3c34c9e0fda9530
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-04-03 04:12:25 -0700

    enabled the app to render a map using google map

--- client/src/place/components/PlaceItem.js
+++ client/src/place/components/PlaceItem.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 
 import Card from "../../shared/components/UI/Card";
+import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UI/Modal";
+import Map from "../../shared/components/UI/Map";
 import "./PlaceItem.css";
 
 const PlaceItem = ({
@@ -12,24 +15,50 @@ const PlaceItem = ({
   creatorId,
   coordinates,
 }) => {
+  const [showMap, setShowMap] = useState(false);
+
+  const openMapHandler = () => {
+    setShowMap(true);
+  };
+
+  const closeMapHandler = () => {
+    setShowMap(false);
+  };
+
   return (
-    <li className="place-item">
-      <Card className="place-item__content">
-        <div className="place-item__image">
-          <img src={image} alt={title} />
+    <Fragment>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <Map center={coordinates} zoom={16} />
         </div>
-        <div className="place-item__info">
-          <h2>{title}</h2>
-          <h3>{address}</h3>
-          <p>{description}</p>
-        </div>
-        <div className="place-item__actions">
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item__image">
+            <img src={image} alt={title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{title}</h2>
+            <h3>{address}</h3>
+            <p>{description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={openMapHandler}>
+              VIEW ON MAP
+            </Button>
+            <Button to={`/places/${id}`}>EDIT</Button>
+            <Button danger>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </Fragment>
   );
 };
 
