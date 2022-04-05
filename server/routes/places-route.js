bb5df926d2112635e6b30f48e505dbb460343805
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-04-05 15:48:04 -0700

    added routes - create/update/delete a place

--- server/routes/places-route.js
+++ server/routes/places-route.js
@@ -5,6 +5,9 @@ const placesController = require("../controllers/places-controller");
 const router = express.Router();
 
 router.get("/:pid", placesController.getPlaceById);
-router.get("/user/:uid", placesController.getPlaceByUserId);
+router.get("/user/:uid", placesController.getPlacesByUserId);
+router.post("/", placesController.createPlace);
+router.patch("/:pid", placesController.updatePlace);
+router.delete("/:pid", placesController.deletePlace);
 
 module.exports = router;
