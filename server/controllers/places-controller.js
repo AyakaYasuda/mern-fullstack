7d02ed4aea8bf2b442de73bf0275b1de07db92bb
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-04-14 00:32:21 -0700

    updated getPlaceByUserId

--- server/controllers/places-controller.js
+++ server/controllers/places-controller.js
@@ -35,9 +35,9 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let places;
+  let userWithPlaces;
   try {
-    places = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate("places");
   } catch (err) {
     const error = new HttpError(
       "Failed to fetch places identified with the provided user Id",
@@ -45,7 +45,7 @@ const getPlacesByUserId = async (req, res, next) => {
     );
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     const error = new HttpError(
       "Could not find a place for the provided user id",
       404
@@ -53,7 +53,11 @@ const getPlacesByUserId = async (req, res, next) => {
     return next(error);
   }
 
-  res.json({ places: places.map(place => place.toObject({ getters: true })) });
+  res.json({
+    places: userWithPlaces.places.map(place =>
+      place.toObject({ getters: true })
+    ),
+  });
 };
 
 const createPlace = async (req, res, next) => {
