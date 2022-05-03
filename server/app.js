271f0ba535b34925459422a64714c4bc54283afe
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-05-03 16:21:49 -0700

    preps for deployment of backend

--- server/app.js
+++ server/app.js
@@ -54,7 +54,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mycluster.xq6fs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(5000, () => {
+    app.listen(process.env.PORT || 5000, () => {
       console.log("Server is running");
     });
   })
